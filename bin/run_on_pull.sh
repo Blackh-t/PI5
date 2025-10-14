1dd572ff1230c24849fbd7ae0974528a021814a1
Narongchai Cherdchoo <149700064+Blackh-t@users.noreply.github.com>
2025-10-14 23:37:17 +0200

    Restart git_pull timer with the newest update

--- bin/run_on_pull.sh
+++ bin/run_on_pull.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-systemctl daamon-load
-systemctl disable webhook.service
+# Load timer to the systemctl
+cd /home/yoshi/git/PI5/systemd/
+cp -f git_pull.timer /etc/systemd/system/
+
+sudo systemctl daamon-load
+sudo systemctl restart git_pull.timer
+sudo systemctl status git_pull.timer >> logger.txt
