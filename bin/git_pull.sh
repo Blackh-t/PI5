4b22ec4527295ecafa46b069777a476f60e7fe03
yoshi <nache7383@uit.no>
2025-10-14 15:03:11 +0200

    NEW::SERVICE Auto git pull 	- Runs script :run_on_pull.sh 		- Can be used to create and activate new services.

---
+++ bin/git_pull.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd
+cd git/PI5
+git pull
+
+# Store git pull output
+GIT_OUTPUT=$(git pull)
+echo "$GIT_OUTPUT"
+
+# Moved the new script to system bin in order to run.
+if [[ "$GIT_OUTPUT" != *"Already up to date."* ]]; then
+    cp -f git/PI5/postman.sh /usr/local/bin/
+    chmod +x /usr/local/bin/run_on_p.sh
+    echo "[UPDATED] run_on_pull.sh "
+else
+    echo "[SKIP] No changed."
+fi
