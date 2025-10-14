bac03b979978d9419659c85f8296d8352c0224d2
blackh-t <nache7383@uit.no>
2025-10-14 15:36:25 +0200

    UPDATED::SCRIPT_PATH

--- bin/git_pull.sh
+++ bin/git_pull.sh
@@ -10,7 +10,7 @@ echo "$GIT_OUTPUT"
 
 # Moved the new script to system bin in order to run.
 if [[ "$GIT_OUTPUT" == *"Fast-forward"* ]]; then
-    cp -f git/PI5/run_on_pull /usr/local/bin/
+    cp -f git/PI5/run_on_pull.sh /usr/local/bin/
     chmod +x /usr/local/bin/run_on_pull.sh
     echo "[UPDATED] run_on_pull.sh "
 else
