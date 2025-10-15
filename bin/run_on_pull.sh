4bf2b43ffc250a602acf96c2404700866244ec23
Narongchai Cherdchoo <149700064+Blackh-t@users.noreply.github.com>
2025-10-15 17:59:16 +0200

    TEST webhook: fan on

--- bin/run_on_pull.sh
+++ bin/run_on_pull.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
 
-# Disable Timer interrupt.
-systemctl daemon-reload
-systemctl disable git_pull.timer
-
-# Load the webhook to the systemd.
-cp -f /home/yoshi/git/PI5/systemd/webhook.service /etc/systemd/system/
-
-# Build the webhook
-cd /home/yoshi/git/PI5/dev/webhook
-cargo build --release
-
-# Change DIR to store the LOG.
-cd /home/yoshi/PI5
-
-# Enable webhook sever.
-systemctl daemon-reload
-systemctl enable webhook.service
-systemctl start --now webhook.service >>logger.txt
-
-# Load PORT forwarding service to the systemd.
-cp -f /home/yoshi/git/PI5/systemd/funnel.service /etc/systemd/system/
-
-# Enable PORT forwarding.
-systemctl daemon-reload
-systemctl enable funnel.service
-systemctl start --now funnel.service >>logger.txt
+pinctrl FAN_PWM op dl
