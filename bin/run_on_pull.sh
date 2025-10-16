f1c3e05f30bdc328cde284e6dd5f19dbe76b91f3
blackh-t <nache7383@uit.no>
2025-10-16 20:20:49 +0200

    SERVICE UPDATE, run_on_pull 	service: +workspace, run by root user.

--- bin/run_on_pull.sh
+++ bin/run_on_pull.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd /home/yoshi/git/PI5/dev/webhook
-cargo build --release
+cd /home/t/git/PI5/systemd/
+cp -f run_on_pull.path /etc/systemd/system/
+cp -f run_on_pull.service /etc/systemd/system/
 
 systemctl daemon-reload
-systemctl restart webhook.service
+systemctl restart run_on_pull.path
+
+systemctl daemon-reload
+systemctl restart run_on_pull.service
