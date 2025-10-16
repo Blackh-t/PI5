05563418c3980b7771b228de37116a0a196c2189
blackh-t <nache7383@uit.no>
2025-10-16 16:30:03 +0200

    SYSTEM UPDATE Webhook sign verification
    	- New ENV in webhook.service: "SECRET_TOKEN"
    	- New Fn, generate_sign() + Test, in Webhook servere

--- bin/run_on_pull.sh
+++ bin/run_on_pull.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+cd /home/yoshi/git/PI5/dev/webhook
+cargo build --release
+
+# cd /home/yoshi/git/PI5/systemd
+# cp -f webhook.service /etc/systemd/system/
+
+# Restart webhook service V1.0.1
+systemctl deamon-reload
+systemctl restart webhook.service
+systemctl status webhook.service >>logger.txt
